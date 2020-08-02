14573ea56604e1f71be44c5120ec8abc6b2ce3b2
Guilherme de Almeida <guilherme.ampp@gmail.com>
2020-08-02 13:20:09 -0300

    [deploy] Deleting node_modules and reinstalling on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,20 @@
 #! /bin/sh
-cd backend/
 sudo killall node
 sudo killall nginx
+
+cd backend/
+rm -rf node_modules/
+rm -rf yarn.lock
+yarn
 sudo nginx -c nginx.conf -p $(pwd)
 yarn start
 cd ..
 
 cd frontend/
 rm -rf build
+rm -rf node_modules/
+rm -rf yarn.lock
+yarn
 yarn build
 yarn add global serve
 serve -s -l 3000 -d build &
